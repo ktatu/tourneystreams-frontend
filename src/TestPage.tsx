5d4fc628d8e5caabb914a8870716b6c3f37fdce7
ktatu <kaikkonentatu@gmail.com>
2023-01-05 01:16:16 +0200

    More work on card component

--- src/TestPage.tsx
+++ src/TestPage.tsx
@@ -1,50 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 
-import { Box, Card, CardActions, CardMedia, CardContent, Tooltip, Typography } from "@mui/material"
+import { Box, Button, Card, CardActions, CardMedia, CardContent, Divider, IconButton, Menu, MenuItem, Tooltip, Typography } from "@mui/material"
 import Grid from "@mui/material/Unstable_Grid2"
-import { Home } from "@mui/icons-material"
+import { ArrowCircleRightOutlined, ArrowForward, HomeOutlined, MoreHoriz } from "@mui/icons-material"
+import TourneyCard from "./components/TourneyCard"
 
 const TestPage = () => {
     return (
-        <Grid container marginTop={30} justifyContent="center">
-            <Card sx={{ maxWidth: 450 }}>
-                <CardMedia
-                    component="img"
-                    height="75"
-                    image={require("./assets/kingscanyon.jpg")}
-                />
-                <CardContent>
-                    <Grid container spacing={1}>
-                        <Grid container>
-                            <Grid xs={8} bgcolor="red">
-                                <Typography gutterBottom variant="h6" component="div">
-                                    Esports Arena: Series E Season 6 Pro Tournament #4
-                                </Typography>
-                            </Grid>
-                            <Grid xs={4} display="flex" justifyContent="flex-end" alignContent="center">
-                                <Typography gutterBottom variant="h6" component="div">
-                                    3d 45min
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid container flexGrow={1} bgcolor="blue">
-                            <Grid xs={8}>
-                                <p>Test: 1</p>
-                                <p>Test: 2</p>
-                            </Grid>
-                            <Grid xs={4}>
-                                <p>test</p>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </CardContent>
-                <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
-                    <a href="https://liquipedia.net/apexlegends/Main_Page" rel="noreferrer" target="_blank">
-                        <img src={require("./assets/liquipedia.png")} width={25} height={20} />
-                    </a>
-                </CardActions>
-            </Card>
+        <Grid container marginTop={30} spacing={5}>
+            <Grid>
+                <TourneyCard tourneyName="Tourney 2"/>
+            </Grid>
         </Grid>
+    )
+}
+// Esports Arena: Series E Season 6 Pro Tournament #4
+
+const DotsMenuItem = () => {
+
+    return (
+        <MenuItem></MenuItem>
     )
 }
 
@@ -65,4 +40,10 @@ export default TestPage
                             </Grid2>
                         </Grid2>
                     </CardContent>
+
+
+                    <a href="https://liquipedia.net/apexlegends/Main_Page" rel="noreferrer" target="_blank">
+                        <img src={require("./assets/liquipedia.png")} width={25} height={20} />
+                    </a>
+
 */

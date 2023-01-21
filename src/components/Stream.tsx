cb921f93e3fee3d84b7b703c9a7aae06ed73790a
ktatu <kaikkonentatu@gmail.com>
2023-01-21 16:51:06 +0200

    Reload stream-button

--- src/components/Stream.tsx
+++ src/components/Stream.tsx
@@ -1,12 +1,18 @@
-import React, { useState, createRef, forwardRef } from "react"
-import { Box, Button } from "@mui/material"
+import React, {
+    useState,
+    createRef,
+    forwardRef,
+    useRef,
+    MutableRefObject,
+} from "react"
+import { Box, Button, Typography } from "@mui/material"
 
 interface EventHandler {
     (argument: string): void
 }
 
 const Stream = () => {
-    const [channels, setChannels] = useState(["imaqtpie"])
+    const [channels, setChannels] = useState(["thijs"])
 
     const handleStreamClose: EventHandler = (channelName) => {
         setChannels(channels.filter((channel) => channel !== channelName))
@@ -32,16 +38,34 @@ const StreamBox = ({
     channel: string
     closeStream: EventHandler
 }) => {
+    const iframeRef = useRef<HTMLIFrameElement>(null)
+
+    const reloadStream = () => {
+        if (iframeRef.current) {
+            // https://stackoverflow.com/questions/86428/what-s-the-best-way-to-reload-refresh-an-iframe/4062084#4062084
+            // eslint-disable-next-line no-self-assign
+            iframeRef.current.src = iframeRef.current.src
+        }
+    }
+
     return (
         <Box
             display="flex"
             flexDirection="column"
+            maxWidth="532px"
+            bgcolor="red"
         >
-            <Box>
-                <Button onClick={() => closeStream(channel)}>Click</Button>
+            <Box
+                display="flex"
+                flexDirection="row"
+            >
+                <Typography sx={{ flexGrow: 1 }}>{channel}</Typography>
+                <Button onClick={reloadStream}>Reload</Button>
+                <Button onClick={() => closeStream(channel)}>Close</Button>
             </Box>
             <Box>
                 <iframe
+                    ref={iframeRef}
                     src={`https://player.twitch.tv/?channel=${channel}&parent=localhost`}
                     style={{ border: 0 }}
                     height="300"

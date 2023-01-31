fe3ecfdf6dbc9439f5be9196828cb6218150842f
ktatu <kaikkonentatu@gmail.com>
2023-01-31 13:58:15 +0200

    Reverting back to not using twitch-react-embed

--- src/shared_components/Streams.tsx
+++ src/shared_components/Streams.tsx
@@ -1,148 +1,95 @@
-import React, { useReducer, useRef, useState } from "react"
-import { Box, Button, IconButton, Skeleton, Typography } from "@mui/material"
+import React, { useRef } from "react"
+import { Box, Button, IconButton, Typography } from "@mui/material"
 import ReplayIcon from "@mui/icons-material/Replay"
 import CloseIcon from "@mui/icons-material/Close"
 import { useSelector } from "react-redux"
 import { ChannelState } from "../reducers/channelReducer"
 import useChannels from "../hooks/useChannels"
-import { TwitchPlayer } from "react-twitch-embed"
-import { TwitchPlayerInstance } from "react-twitch-embed/dist/types"
-import { createSelector } from "@reduxjs/toolkit"
-import { RootState } from "../store"
 
 interface EventHandler {
     (argument: string): void
-}
-
-enum StreamStatus {
-    Online = "ONLINE",
-    Offline = "OFFLINE",
-    Unknown = "UNKNOWN",
 }
 
 const Streams = () => {
     const channels = useSelector(({ channels }: { channels: ChannelState }) => {
         return channels.selectedChannels
     })
-
     const channelsHook = useChannels()
 
     const handleStreamClose: EventHandler = (channelName) => {
         channelsHook.removeStream(channelName)
     }
 
+    console.log("rerender")
+
     return (
         <Box
             display="flex"
             flexDirection="row"
             flexWrap="wrap"
-            gap="10px"
         >
-            {channels.map((channel: string, index: number) => (
+            {channels.map((channel: string) => (
                 <StreamContainer
                     key={channel}
                     channel={channel}
                     closeStream={handleStreamClose}
-                    id={index.toString()}
                 />
             ))}
         </Box>
     )
 }
 
-const makeSelectChannel = () =>
-    createSelector(
-        (state: RootState) => state.channels.selectedChannels,
-        (_: any, channelName: string) => channelName,
-        (selectedChannels, channelName) =>
-            selectedChannels.find((channel: string) => channel === channelName)
-    )
-
 const StreamContainer = ({
     channel,
     closeStream,
-    id,
 }: {
     channel: string
     closeStream: EventHandler
-    id: string
 }) => {
-    const [streamStatus, setStreamStatus] = useState<StreamStatus>(StreamStatus.Unknown)
-    const [_, forceUpdate] = useReducer((x) => x + 1, 0)
-    const playerRef = useRef<TwitchPlayerInstance>()
-
-    const channelsHook = useChannels()
-
-    const handleStreamClose: EventHandler = (channel) => {
-        channelsHook.removeStream(channel)
-    }
+    const iframeRef = useRef<HTMLIFrameElement>(null)
 
     const reloadStream = () => {
-        // https://reactjs.org/docs/hooks-faq.html#is-there-something-like-forceupdate
-        // I haven't found a way to reload the actual stream using TwitchPlayer, so rerender the component instead
-        forceUpdate()
-    }
-
-    const handlePlayerReady = (playerInstance: TwitchPlayerInstance) => {
-        playerRef.current = playerInstance
-    }
-
-    const handleChannelStatus = (newStatus: StreamStatus) => {
-        if (playerRef.current) {
-            setStreamStatus(newStatus)
+        if (iframeRef.current) {
+            // https://stackoverflow.com/questions/86428/what-s-the-best-way-to-reload-refresh-an-iframe/4062084#4062084
+            // eslint-disable-next-line no-self-assign
+            iframeRef.current.src = iframeRef.current.src
         }
     }
 
-    /*
-    if (streamStatus === "OFFLINE") {
-        closeStream(channel)
-        return null
-    }*/
-
     return (
-        <Box>
-            <Box display={streamStatus === "UNKNOWN" ? "flex" : "none"}>
-                <Skeleton
-                    variant="rectangular"
-                    width="532px"
-                    height="300px"
-                />
-            </Box>
+        <Box
+            display="flex"
+            flexDirection="column"
+            maxWidth="532px"
+            bgcolor="red"
+        >
             <Box
-                display={streamStatus === "ONLINE" ? "flex" : "none"}
-                flexDirection="column"
-                maxWidth="532px"
-                bgcolor="red"
+                display="flex"
+                flexDirection="row"
             >
-                <Box
-                    display="flex"
-                    flexDirection="row"
+                <Typography
+                    marginTop={1}
+                    sx={{ flexGrow: 1 }}
                 >
-                    <Typography
-                        marginTop={1}
-                        sx={{ flexGrow: 1 }}
-                    >
-                        {channel}
-                    </Typography>
-                    <IconButton onClick={reloadStream}>
-                        <ReplayIcon />
-                    </IconButton>
-                    <IconButton onClick={() => handleStreamClose(channel)}>
-                        <CloseIcon />
-                    </IconButton>
-                </Box>
-                <Box>
-                    <TwitchPlayer
-                        id={id}
-                        width={532}
-                        height={300}
-                        channel={channel}
-                        muted
-                        onOffline={() => handleChannelStatus(StreamStatus.Offline)}
-                        onOnline={() => handleChannelStatus(StreamStatus.Online)}
-                        onReady={handlePlayerReady}
-                    />
-                </Box>
+                    {channel}
+                </Typography>
+                <IconButton onClick={reloadStream}>
+                    <ReplayIcon />
+                </IconButton>
+                <IconButton onClick={() => closeStream(channel)}>
+                    <CloseIcon />
+                </IconButton>
+            </Box>
+            <Box>
+                <iframe
+                    ref={iframeRef}
+                    src={`https://player.twitch.tv/?channel=${channel}&muted=true&parent=localhost`}
+                    style={{ border: 0 }}
+                    height="300"
+                    width="532"
+                    title="stream"
+                    allowFullScreen
+                ></iframe>
             </Box>
         </Box>
     )

0a071020f5a59b59755dee42e0f3d4dcc5683068
ktatu <kaikkonentatu@gmail.com>
2023-02-28 03:14:03 +0200

    clean up

--- src/home_page/Layout.tsx
+++ src/home_page/Layout.tsx
@@ -1,36 +1,9 @@
 import { Box, Unstable_Grid2 as Grid, Stack } from "@mui/material"
-import TourneyCarousel from "./TourneyCarousel"
 import Streams from "../shared_components/Streams"
 import Chats from "../shared_components/Chats"
 import TourneyDrawer from "../TourneyDrawer"
 
 const Layout = () => {
-    /*
-    return (
-        <Grid
-            container
-            spacing={2}
-            paddingTop={2}
-            paddingRight={1}
-            xs={9}
-            bgcolor="red"
-        >
-            <Grid
-                xs={10}
-                bgcolor="purple"
-            >
-                <Box
-                    overflow="auto"
-                    height="800px"
-                >
-                    <Stack spacing={5}>
-                        <Streams />
-                    </Stack>
-                </Box>
-            </Grid>
-        </Grid>
-    )*/
-
     return (
         <Box
             display="flex"

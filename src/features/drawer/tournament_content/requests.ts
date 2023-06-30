3a3c7b717bef67b581b5b78140ec9c2f7ef58978
ktatu <kaikkonentatu@gmail.com>
2023-06-30 16:00:09 +0300

    json test-server from port 3001 to 3002

--- src/features/drawer/tournament_content/requests.ts
+++ src/features/drawer/tournament_content/requests.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { TourneyInfo } from "./types"
 
-const BASE_URL = "http://localhost:3001/tourneyInfos"
+const BASE_URL = "http://localhost:3002/tourneyInfos"
 
 export const getTourneyInfos = () => {
     return axios.get<TourneyInfo[]>(BASE_URL)

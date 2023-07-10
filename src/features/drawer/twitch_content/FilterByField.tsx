fd358be110429c8812a974d0e827704d153020bf
ktatu <kaikkonentatu@gmail.com>
2023-07-10 16:28:54 +0300

    Filter stream field changes

--- src/features/drawer/twitch_content/FilterByField.tsx
+++ src/features/drawer/twitch_content/FilterByField.tsx
@@ -1,8 +1,9 @@
-import { TextField, MenuList, MenuItem } from "@mui/material"
+import { TextField, MenuList, MenuItem, Box, IconButton } from "@mui/material"
 import { startTransition, useRef } from "react"
 import { FilterBy } from "."
 import PopupMenu, { PopupMenuClose } from "../../../commons/PopupMenu"
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
+import CloseIcon from "@mui/icons-material/Close"
 
 interface FilterByFieldProps {
     filterValue: string
@@ -34,6 +35,7 @@ const FilterByField = ({
 
     return (
         <TextField
+            autoComplete="off"
             label={`Filter by: ${filterType}`}
             InputLabelProps={{ shrink: true }}
             onChange={handleFilterChange}
@@ -41,31 +43,41 @@ const FilterByField = ({
             sx={{ maxWidth: "200px" }}
             InputProps={{
                 endAdornment: (
-                    <PopupMenu
-                        buttonProps={{ buttonIcon: <ArrowDropDownIcon /> }}
-                        ref={popupMenuRef}
-                    >
-                        <MenuList>
-                            <MenuItem
-                                onClick={() => handleFilterTypeChange(FilterBy.Category)}
-                                selected={filterType === FilterBy.Category}
-                            >
-                                category
-                            </MenuItem>
-                            <MenuItem
-                                onClick={() => handleFilterTypeChange(FilterBy.ChannelName)}
-                                selected={filterType === FilterBy.ChannelName}
-                            >
-                                channel name
-                            </MenuItem>
-                            <MenuItem
-                                onClick={() => handleFilterTypeChange(FilterBy.Title)}
-                                selected={filterType === FilterBy.Title}
-                            >
-                                stream title
-                            </MenuItem>
-                        </MenuList>
-                    </PopupMenu>
+                    <Box display="flex">
+                        <IconButton
+                            onClick={() => setFilterValue("")}
+                            sx={{
+                                visibility: filterValue ? "visible" : "hidden",
+                            }}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                        <PopupMenu
+                            buttonProps={{ buttonIcon: <ArrowDropDownIcon /> }}
+                            ref={popupMenuRef}
+                        >
+                            <MenuList>
+                                <MenuItem
+                                    onClick={() => handleFilterTypeChange(FilterBy.Category)}
+                                    selected={filterType === FilterBy.Category}
+                                >
+                                    category
+                                </MenuItem>
+                                <MenuItem
+                                    onClick={() => handleFilterTypeChange(FilterBy.ChannelName)}
+                                    selected={filterType === FilterBy.ChannelName}
+                                >
+                                    channel name
+                                </MenuItem>
+                                <MenuItem
+                                    onClick={() => handleFilterTypeChange(FilterBy.Title)}
+                                    selected={filterType === FilterBy.Title}
+                                >
+                                    stream title
+                                </MenuItem>
+                            </MenuList>
+                        </PopupMenu>
+                    </Box>
                 ),
             }}
         />

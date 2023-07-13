ec96d3dd6af75262d3d2f9d617616453949f5d15
ktatu <kaikkonentatu@gmail.com>
2023-07-13 13:47:11 +0300

    Rework stream frames handling

--- src/features/main_content/StreamFrames.tsx
+++ src/features/main_content/StreamFrames.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react"
+import { memo, useEffect, useRef } from "react"
 import { Box } from "@mui/material"
 import { useStreamContext } from "../../commons/streamReducer"
 import getStreamDimensions from "./streamDimensions"
@@ -8,68 +8,42 @@ import StreamFrameContainer from "./StreamFrameContainer"
 const StreamFrames = () => {
     const { streamState } = useStreamContext()
 
-    const invisibleButtonRef = useRef<HTMLButtonElement>(null)
-
     const STREAM_BASEWIDTH = 1500
     const STREAM_BASEHEIGHT = 875
-
-    useEffect(() => {
-        const handleStreamFocus = () => {
-            if (!document.hidden && invisibleButtonRef.current) {
-                invisibleButtonRef.current.focus()
-            }
-        }
-
-        document.addEventListener("visibilitychange", handleStreamFocus)
-
-        return () => {
-            document.removeEventListener("visibilitychange", handleStreamFocus)
-        }
-    }, [])
 
     return (
         <Box
             display="flex"
-            flexDirection="column"
-            paddingLeft={1}
+            flexDirection="row"
+            flexWrap="wrap"
+            justifyContent="center"
+            alignContent="center"
+            bgcolor="red"
         >
-            <button
-                style={{ backgroundColor: "transparent", border: "none" }}
-                ref={invisibleButtonRef}
-            ></button>
-            <Box
-                display="flex"
-                flexDirection="row"
-                flexWrap="wrap"
-                justifyContent="center"
-                alignContent="center"
-                bgcolor="red"
-            >
-                {streamState.streams.map((stream, index) => {
-                    const { width, height } = getStreamDimensions(
-                        streamState.streams.length,
-                        stream.displayPosition,
-                        STREAM_BASEWIDTH,
-                        STREAM_BASEHEIGHT
-                    )
+            {streamState.streams.map((stream, index) => {
+                const { width, height } = getStreamDimensions(
+                    streamState.streams.length,
+                    stream.displayPosition,
+                    STREAM_BASEWIDTH,
+                    STREAM_BASEHEIGHT
+                )
 
-                    return (
-                        <Box
+                return (
+                    <Box
+                        key={index}
+                        order={stream.displayPosition}
+                    >
+                        <StreamFrameContainer
                             key={index}
-                            order={stream.displayPosition}
-                        >
-                            <StreamFrameContainer
-                                key={index}
-                                channel={stream.channelName}
-                                frameWidth={width}
-                                frameHeight={height}
-                            />
-                        </Box>
-                    )
-                })}
-            </Box>
+                            channel={stream.channelName}
+                            frameWidth={width}
+                            frameHeight={height}
+                        />
+                    </Box>
+                )
+            })}
         </Box>
     )
 }
 
-export default StreamFrames
+export default memo(StreamFrames)

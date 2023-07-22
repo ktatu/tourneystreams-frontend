789dc695bf4d100f7142df02fff99340a2569637
ktatu <kaikkonentatu@gmail.com>
2023-07-22 21:20:56 +0300

    Displaying > 3 streams on app bar removed

--- src/features/app_bar/StreamList.tsx
+++ src/features/app_bar/StreamList.tsx
@@ -21,6 +21,7 @@ const StreamList = () => {
 
     // layouteffect and firstComponentRender prevent showing a swap from list to menu to user on first render
     // relevant only when the url is loaded with > 3 streams in search parameters
+    /*
     useLayoutEffect(() => {
         if (streamState.streams.length > 3) {
             setShowHorizontalList(false)
@@ -39,7 +40,7 @@ const StreamList = () => {
         } else {
             setShowMenuButton(false)
         }
-    }, [streamState.streams, anchorEl])
+    }, [streamState.streams, anchorEl])*/
 
     const handleMenuOpen: MouseEventHandler = (event: React.MouseEvent<HTMLElement>): void => {
         setAnchorEl(event.currentTarget)
@@ -52,92 +53,41 @@ const StreamList = () => {
     return (
         <>
             <Box
-                ref={slideContainerRef}
-                overflow="hidden"
-                position="relative"
-                minWidth="800px"
-                minHeight="50px"
+                width="200px"
+                height="50px"
+                display="flex"
             >
                 <Box
-                    width="700px"
-                    height="50px"
-                    position="absolute"
                     display="flex"
+                    alignItems="center"
                 >
-                    <DragAndDropWrapper
-                        movementAxis={MovementAxis.Horizontal}
-                        sortableItems={sortedChannels}
+                    <Button
+                        variant="contained"
+                        onClick={handleMenuOpen}
+                        endIcon={<KeyboardArrowDownIcon />}
                     >
-                        <Slide
-                            appear={false}
-                            direction="right"
-                            in={showHorizontalList}
-                            container={slideContainerRef.current}
-                            timeout={firstComponentRender ? 0 : 500}
-                            onExited={() => setShowMenuButton(true)}
+                        Streams
+                    </Button>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={anchorEl !== null}
+                        onClose={handleMenuClose}
+                    >
+                        <DragAndDropWrapper
+                            movementAxis={MovementAxis.Vertical}
+                            sortableItems={sortedChannels}
                         >
-                            <Box
-                                display="flex"
-                                gap={2}
-                            >
+                            <Stack direction="column">
                                 {sortedChannels.map((channel) => (
                                     <StreamListItem
                                         key={channel}
                                         channel={channel}
-                                        oneStreamOpen={streamState.streams.length === 1}
+                                        oneStreamOpen={false}
                                     />
                                 ))}
-                            </Box>
-                        </Slide>
-                    </DragAndDropWrapper>
-                </Box>
-                <Box
-                    width="200px"
-                    height="50px"
-                    position="absolute"
-                    display="flex"
-                >
-                    <Slide
-                        appear={false}
-                        direction="right"
-                        in={showMenuButton}
-                        container={slideContainerRef.current}
-                        timeout={firstComponentRender ? 0 : 500}
-                        onExited={() => setShowHorizontalList(true)}
-                    >
-                        <Box
-                            display="flex"
-                            alignItems="center"
-                        >
-                            <Button
-                                variant="contained"
-                                onClick={handleMenuOpen}
-                                endIcon={<KeyboardArrowDownIcon />}
-                            >
-                                Streams
-                            </Button>
-                            <Menu
-                                anchorEl={anchorEl}
-                                open={anchorEl !== null}
-                                onClose={handleMenuClose}
-                            >
-                                <DragAndDropWrapper
-                                    movementAxis={MovementAxis.Vertical}
-                                    sortableItems={sortedChannels}
-                                >
-                                    <Stack direction="column">
-                                        {sortedChannels.map((channel) => (
-                                            <StreamListItem
-                                                key={channel}
-                                                channel={channel}
-                                                oneStreamOpen={false}
-                                            />
-                                        ))}
-                                    </Stack>
-                                </DragAndDropWrapper>
-                            </Menu>
-                        </Box>
-                    </Slide>
+                            </Stack>
+                        </DragAndDropWrapper>
+                    </Menu>
                 </Box>
             </Box>
         </>

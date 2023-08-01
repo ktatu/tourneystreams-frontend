c9daa917b8369f2c4fc7a885a5f68ffadcc5c01e
ktatu <kaikkonentatu@gmail.com>
2023-08-01 18:09:53 +0300

    Update Chat.tsx

--- src/features/main_content/Chat.tsx
+++ src/features/main_content/Chat.tsx
@@ -7,9 +7,11 @@ const Chat = () => {
         return null
     }
 
+    console.log("window location host name ", window.location.hostname)
+
     return (
         <iframe
-            src={`https://www.twitch.tv/embed/${streamState.selectedChannel}/chat?darkpopout&parent=localhost`}
+            src={`https://www.twitch.tv/embed/${streamState.selectedChannel}/chat?darkpopout&parent=${window.location.hostname}`}
             style={{
                 display: "block",
                 border: "none",

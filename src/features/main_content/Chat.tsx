718dd2beb50e99541c326c2e816bfd926f3f5fe8
ktatu <kaikkonentatu@gmail.com>
2023-08-04 02:06:31 +0300

    Display channel name in Chat

--- src/features/main_content/Chat.tsx
+++ src/features/main_content/Chat.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material"
 import { useStreamContext } from "../../commons/streamReducer"
 
 const Chat = () => {
@@ -7,18 +8,34 @@ const Chat = () => {
         return null
     }
 
-    console.log("window location host name ", window.location.hostname)
-
     return (
-        <iframe
-            src={`https://www.twitch.tv/embed/${streamState.selectedChannel}/chat?darkpopout&parent=${window.location.hostname}`}
-            style={{
-                display: "block",
-                border: "none",
-            }}
-            height="100%"
+        <Box
             width="340px"
-        ></iframe>
+            height="100%"
+            position="relative"
+        >
+            <iframe
+                src={`https://www.twitch.tv/embed/${streamState.selectedChannel}/chat?darkpopout&parent=${window.location.hostname}`}
+                style={{
+                    display: "block",
+                    border: "none",
+                }}
+                height="100%"
+                width="100%"
+            ></iframe>
+            <Box
+                position="absolute"
+                bottom={0}
+                right={100}
+                width={100}
+                height={50}
+                display="flex"
+                alignContent="center"
+                alignItems="center"
+            >
+                {streamState.selectedChannel}
+            </Box>
+        </Box>
     )
 }
 

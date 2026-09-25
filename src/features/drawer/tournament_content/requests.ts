989d804a86b4675a627c61737b3786082a999df5
ktatu <kaikkonentatu@gmail.com>
2026-09-25 18:22:56 +0300

    Removing tournaments feature

--- src/features/drawer/tournament_content/requests.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-import { TourneyInfo } from "./types"
-
-const BASE_URL = "http://localhost:3002/tourneyInfos"
-
-export const getTourneyInfos = () => {
-    return axios.get<TourneyInfo[]>(BASE_URL)
-}
--- src/features/drawer/tournament_content/useGameFilter.ts
+++
@@ -1,9 +0,0 @@
-import { Game, TourneyInfo } from "./types"
-
-const useGameFilter = (tourneyInfoArray: TourneyInfo[]) => {
-    const PLACE_HOLDER_GAMES: Game[] = [Game.ApexLegends, Game.Valorant]
-
-    return tourneyInfoArray.filter((tourneyInfo) => PLACE_HOLDER_GAMES.includes(tourneyInfo.game))
-}
-
-export default useGameFilter
